32940336e7d83dcc626a24c68e42a0aab7d5f38a
KevinOluoch <Kevin.o.oluoch@gmail.com>
2019-08-26 16:37:09 +0300

    Use tmap library for map instead of leaflet
    Also switch position of tables: "Wards in Selected District" and "Highligted Ward(s)"

--- app.R
+++ app.R
@@ -9,7 +9,9 @@ library(raster)
 library(rgdal)
 
 library(DT)
-library(leaflet)
+library(tmap)
+library(grid)
+#library(leaflet)
 
 # Used to create North arrow
 # library(maptools)
@@ -18,7 +20,10 @@ library(leaflet)
 
 #### DATA
 # Load PH raster and wards shapefile
-TZA_SoilPH <- raster("./data/PH.tif")
+#TZA_SoilPH <- raster("./data/PH.tif")
+TZA_level0 <- shapefile("./data/gadm36_TZA_0.shp")
+TZA_level1 <- shapefile("./data/gadm36_TZA_1.shp")
+TZA_level2 <- shapefile("./data/gadm36_TZA_2.shp")
 TZA_level3 <- shapefile("./data/TZA_level3_data.shp")
 
 # Prepare the Data
@@ -26,21 +31,10 @@ TZA_level3 <- shapefile("./data/TZA_level3_data.shp")
 TZA_level3@data$AREA_sq <- round(TZA_level3@data$AREA_sq, digits = 2)
 TZA_level3@data$Crp_Cv_ <- round(TZA_level3@data$Crp_Cv_, digits = 2)
 TZA_level3@data$PH <- round(TZA_level3@data$PH, digits = 2)
-names(TZA_level3) <- c("Region", "Disrict", "Ward", "AREA_sqkm", "Crop Cover_perc", "PH")
+names(TZA_level3) <- c("Region", "District", "Ward", "AREA_sqkm", "Crop Cover_perc", "PH")
 # Create a map wiget
-TZA_level3_leaflet <- leaflet(TZA_level3, options = leafletOptions(minZoom = 5.5, maxZoom = 10)) %>% 
-  setView(lng = 35, lat = -6, zoom = 7) %>% 
-  addTiles() %>% 
-  addPolygons(layerId = 1,
-              color = "#444444",
-              weight = 1,
-              smoothFactor = 0.5,
-              opacity = 0.5, 
-              fillOpacity = 0.1,
-              #fillColor = "brown", #~colorQuantile("YlOrRd", Disrict)(Disrict),
-              highlightOptions = highlightOptions(color = "white", 
-                                                  weight = 2,
-                                                  bringToFront = TRUE))
+TZA_level1_tm <- tm_shape(TZA_level1) + tm_borders() 
+# + tm_text("Region") 
 
 
 ##### USER INTERFACE
@@ -57,26 +51,26 @@ ui <- dashboardPage(
           selectInput("region", "Select Region", unique(TZA_level3@data$Region)),
           uiOutput("districtUI"),
           actionButton("zoom", "Show Wards")
-          ),
+      ),
       # ui2:check serv2
       box(title = "Selected Tanzania Wards",
-          leafletOutput("picked_wards")
-          )
+          plotOutput("picked_wards")
+      )
     ),
     
     # Information on wards in selected district
     fluidRow( 
       column = 6,
+      # ui4:check serv4
+      box(title = "Wards in Selected District",
+          DTOutput('wards_tbl')
+          ),
       # ui3:check serv3
       box(title = "Highligted Ward(s)",
           #verbatimTextOutput("picked_rows")
           DTOutput("picked_rows"),
           downloadLink('downloadCSV', 'Download CSV |')
           # downloadLink('downloadSHP', '| Download shapefile')
-          ),
-      # ui4:check serv4
-      box(title = "Wards in Selected District",
-          DTOutput('wards_tbl')
           )
       )
     )
@@ -91,14 +85,14 @@ server <- function(input, output, session) {
   output$districtUI = renderUI({
     choices_district = TZA_level3@data[is.element(TZA_level3@data$Region, 
                                                   input$region), 
-                                       "Disrict"]
+                                       "District"]
     selectInput("district", "Select District", choices_district, selected = choices_district[1])
   })
   
   # serv2: outputs to ui2 
   # Display information on wards in the selected district in a Table
   wards_in_district <- eventReactive(input$zoom, 
-                                     {TZA_level3@data[is.element(TZA_level3@data$Disrict,
+                                     {TZA_level3@data[is.element(TZA_level3@data$District,
                                                                  input$district
                                      ),
                                      ]
@@ -109,7 +103,7 @@ server <- function(input, output, session) {
   # serv3: outputs to ui3 
   # Show more details on wards highligted in the table
   output$picked_rows <- renderDT({
-    slctd_dscts <- TZA_level3[is.element(TZA_level3@data$Disrict,input$district),]
+    slctd_dscts <- TZA_level3[is.element(TZA_level3@data$District,input$district),]
     slctd_dscts <- slctd_dscts[input$wards_tbl_rows_selected,]
     #print(input$wards_tbl_rows_selected)
     slctd_dscts@data
@@ -121,34 +115,24 @@ server <- function(input, output, session) {
              gsub("-", "", Sys.Date(), fixed = TRUE), "_",
              gsub(":", "", format(Sys.time(), "%H:%M:%S"), fixed = TRUE),
              '.csv'
-            )
+      )
     },
     content = function(con) {
-      slctd_dscts_data <- TZA_level3[is.element(TZA_level3@data$Disrict,input$district),]
+      slctd_dscts_data <- TZA_level3[is.element(TZA_level3@data$District,input$district),]
       slctd_dscts_data <- slctd_dscts_data[input$wards_tbl_rows_selected,]
       write.csv(slctd_dscts_data@data, con)
     }
   )
-  # output$downloadSHP <- downloadHandler(
-  #   filename = function() {
-  #     paste('data-', Sys.Date(), '.shp', sep='')
-  #   },
-  #   content = function(con) {
-  #     slctd_dscts_data <- TZA_level3[is.element(TZA_level3@data$Disrict,input$district),]
-  #     slctd_dscts_data <- slctd_dscts_data[input$wards_tbl_rows_selected,]
-  #     shapefile(slctd_dscts_data, con)
-  #   }
-  # )
-
+  
   # serv4: outputs to ui4 
   # Select a district to focus on 
   selected_district <- eventReactive(
     input$zoom,
     {
       # Get the Selected district extent
-      slctd_dscts <- TZA_level3[is.element(TZA_level3@data$Disrict,input$district),]
+      slctd_dscts <- TZA_level3[is.element(TZA_level3@data$District,input$district),]
       district_extent <- extent(slctd_dscts)
-      distWardsCol <- colorFactor(topo.colors(length(TZA_level3)), slctd_dscts@data$Ward)
+      # distWardsCol <- colorFactor(topo.colors(length(TZA_level3)), slctd_dscts@data$Ward)
       
       # Create list of popup labels
       labels_list <- lapply(seq(nrow(slctd_dscts@data)), function(i) {
@@ -159,33 +143,26 @@ server <- function(input, output, session) {
       })
       
       # Add Selected districts to the map widget
-      slctd_dscts_map <- TZA_level3_leaflet %>% 
-        addPolygons(data = slctd_dscts,
-                    layerId = 2,
-                    color = "#444444",
-                    label = lapply(labels_list, htmltools::HTML),
-                    weight = 1,
-                    smoothFactor = 0.5,
-                    opacity = 1.0, 
-                    fillOpacity = 0.2,
-                    fillColor = ~distWardsCol(Ward),
-                    highlightOptions = highlightOptions(color = "white", 
-                                                        weight = 2,
-                                                       bringToFront = TRUE)) 
-      # Zoom to the selected district in a map
-      slctd_dscts_map %>%
-        fitBounds(district_extent[1],
-                  district_extent[3],
-                  district_extent[2],
-                  district_extent[4]) %>%
-        addMiniMap() %>% addScaleBar(position = "bottomleft") %>% addGraticule(interval = 1, 
-                                                                               style = list(color = "brown", 
-                                                                                            weight = 1))
-      }
-    )
+      dist_bbox <- bbox(slctd_dscts)
+      district_outline <- st_bbox(c(xmin = xx[1], xmax = xx[3], ymin = xx[2], ymax = xx[4]), 
+                                  crs = st_crs(slctd_dscts)) %>% st_as_sfc()
+      insetmap <- TZA_level1_tm + tm_text("NAME_1", size = 0.75) + tm_shape(slctd_dscts) + tm_fill(col = "red")  
+      mainmap <- tm_shape(slctd_dscts) + tm_borders() + TZA_level1_tm + 
+                 tm_shape(slctd_dscts,) + tm_borders() + 
+                 tm_fill(col = "PH",
+                         popup.vars = c("Ward", "AREA_sqkm")
+                         ) +
+                  tm_text("Ward", size = 0.75) +
+                  tm_compass(type = "8star", position = c("left", "top")) +
+                  tm_scale_bar(width = 0.5)
+       # tmap_mode("plot") 
+      tmap_arrange(mainmap, insetmap)
+      
+    }
+  )
   # Render Dynamic map
-  output$picked_wards <- renderLeaflet(selected_district())
-
+  output$picked_wards <- renderPlot(selected_district())
+  
   
 }
 
